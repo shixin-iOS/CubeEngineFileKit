7bb73e086dc7e080e5a6a08cd1426a2876ab1092
shixin-iOS <44627446+shixin-iOS@users.noreply.github.com>
2019-04-17 17:53:12 +0800

    Add files via upload

---
+++ CubeEngineFileKit.framework/Headers/CubeEngineFileKit.h
@@ -0,0 +1,25 @@
+//
+//  CubeEngineFileKit.h
+//  CubeEngineFileKit
+//
+//  Created by jianchengpan on 2019/3/22.
+//  Copyright © 2019 CubeTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CubeEngineFileKit.
+FOUNDATION_EXPORT double CubeEngineFileKitVersionNumber;
+
+//! Project version string for CubeEngineFileKit.
+FOUNDATION_EXPORT const unsigned char CubeEngineFileKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CubeEngineFileKit/PublicHeader.h>
+
+#import <CubeEngineFoundationKit/CubeEngineFoundationKit.h>
+
+//FileService
+#import <CubeEngineFileKit/CubeFileInfo.h>
+#import <CubeEngineFileKit/UploadCubeFile.h>
+#import <CubeEngineFileKit/DownloadCubeFile.h>
+
